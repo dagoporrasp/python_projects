77b400330ad3507713505c7d43b5bb0ecb62d7e3
dagoporrasp <54688722+dagoporrasp@users.noreply.github.com>
2019-09-02 12:18:53 -0500

    He puesto la funcionalidad de responder
    Se han agregado dos respuestas el saludo que contenga "hola", y si le preguntas "Cómo te llamas", a lo cual responderá

--- main.py
+++ main.py
@@ -11,4 +11,33 @@ def speak(text):
 	tts.save(filename)
 	playsound.playsound(filename)
 
-speak("hola Dago")
+def get_audio():
+	# obtain audio from the microphone
+	r = sr.Recognizer()
+	with sr.Microphone() as source:
+		print("Di algo!")
+		audio = r.listen(source)
+		said = ""
+		try:
+			said = r.recognize_google(audio, language = "es")
+			print(said)
+		except Exception as e:
+			print("Exception: "+ str(e))
+	return said
+
+def main():
+	print(".....")
+	text = get_audio()
+
+	if "Hola" in text:
+		speak("¡Hola Dago!, ¿Cómo estás?")
+
+	if "Cómo te llamas" in text:
+		speak("Mi nombre es D.A.I")
+
+	# speak("hola Dago")
+	
+	# said = get_audio()
+
+if __name__ == '__main__':
+	main()
